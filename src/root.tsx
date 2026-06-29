585aac89307eb8e7728e13b59990d06ce02f8a79
kerols badr tawfik zaki <59099087+krrish-dev@users.noreply.github.com>
2026-06-29 09:27:53 +0300

    Remove fixed body language for localized routes

--- src/root.tsx
+++ src/root.tsx
@@ -24,7 +24,7 @@ export default component$(() => {
         )}
         <RouterHead />
       </head>
-      <body lang="en">
+      <body>
         <RouterOutlet />
       </body>
     </QwikCityProvider>
